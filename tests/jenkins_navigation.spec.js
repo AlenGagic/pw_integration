9f6c5d9f223e846e3aa4f7c94f454fb15874a405
Alen Gagic <alen.gagic@Helveticode.Local>
2024-01-25 09:32:56 +0100

    first commit -missing jenkins

---
+++ tests/jenkins_navigation.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@playwright/test');
+
+
+test ("Testing simple navigation for Jenkins integration", async ({ page }) => {
+
+    const homePage = "http://localhost:3000/";
+    const loginButton = page.locator(".login-control-button.nav-link");
+
+    await page.goto(homePage);
+
+    await loginButton.waitFor();
+
+    await loginButton.click();
+
+    await page.waitForTimeout(10000);
+
+    await page.close();
+})
